be1a3af50ce8f7440103600782a9b35c70543c49
perchas04 <166369652+perchas04@users.noreply.github.com>
2024-04-24 17:50:44 +0200

    calculadora.java

--- calculadora_1/src/main/java/jorgelosada/calculadora_1/Calculadora_1.java
+++ calculadora_1/src/main/java/jorgelosada/calculadora_1/Calculadora_1.java
@@ -1,16 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package jorgelosada.calculadora_1;
-
-/**
- *
- * @author jorge
- */
-public class Calculadora_1 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+//hola soy el jorge flipas
