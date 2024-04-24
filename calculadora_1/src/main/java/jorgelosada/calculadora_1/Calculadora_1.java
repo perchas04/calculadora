a60bf320637add3f2a57df9f0fe5df8f67e7ae3e
perchas04 <166369652+perchas04@users.noreply.github.com>
2024-04-24 17:41:45 +0200

    Add files via upload

---
+++ calculadora_1/src/main/java/jorgelosada/calculadora_1/Calculadora_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package jorgelosada.calculadora_1;
+
+/**
+ *
+ * @author jorge
+ */
+public class Calculadora_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
